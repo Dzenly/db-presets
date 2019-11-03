ef67987aa2dac15963a5460b5aa960fbcc7e4509
Dzenly <alexchem@yandex.ru>
2019-11-04 01:22:03 +0700

    Finished first draft of design.

--- log/logger.js
+++ log/logger.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const winston = require('winston');
+const logPrefixer = require('../index.js');
+
+/**
+ * logger = new winston.Logger({
+    transports: [
+      new winston.transports.Console({
+        level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
+        colorize: true,
+        timestamp: false,
+        prettyPrint: true,
+        showLevel: true,
+      }),
+    ],
+  });
+ */
+
+/**
+ * Returns logger.
+ * @param {String} prefix - All logged strings will be prefixed by this value.
+ * @return {Object} - logger.
+ */
+module.exports = function getLogger(prefix) {
+  return logPrefixer.addPrefixToCommonLogFuncs(winston, prefix);
+};
