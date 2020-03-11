4f5a2745c451ab44aa73f0c8d7e2a3156ba6b0ff
barslev <benjaminbarslev@gmail.com>
2020-03-11 14:17:46 +0100

    Code cleanup

--- logger/logger.js
+++ logger/logger.js
@@ -9,54 +9,14 @@ const logPrefixer = require('dz-log-prefix');
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console((function(opts) {
-          const newOpts = {};
-          const formatArray = [];
-          const formatOptions = {
-            stringify: () => winston.format((info) => { info.message = JSON.stringify(info.message); })(),
-            formatter: () => winston.format((info) => { info.message = opts.formatter(Object.assign(info, opts)); })(),
-            json: () => winston.format.json(),
-            raw: () => winston.format.json(),
-            label: () => winston.format.label(opts.label),
-            logstash: () => winston.format.logstash(),
-            prettyPrint: () => winston.format.prettyPrint({depth: opts.depth || 2}),
-            colorize: () => winston.format.colorize({level: opts.colorize === true || opts.colorize === 'level', all: opts.colorize === 'all', message: opts.colorize === 'message'}),
-            timestamp: () => winston.format.timestamp(),
-            align: () => winston.format.align(),
-            showLevel: () => winston.format((info) => { info.message = info.level + ': ' + info.message; })()
-          }
-          Object.keys(opts).filter(k => !formatOptions.hasOwnProperty(k)).forEach((k) => { newOpts[k] = opts[k]; });
-          Object.keys(opts).filter(k => formatOptions.hasOwnProperty(k) && formatOptions[k]).forEach(k => formatArray.push(formatOptions[k]()));
-          newOpts.format = winston.format.combine(...formatArray);
-          return newOpts;
-        })({
+    new winston.transports.Console(convertOptionsToWinstonV3({
       level: process.env.DBP_CON_LOG_LEVEL || 'info',
       colorize: true,
       timestamp: false,
       prettyPrint: true,
       showLevel: true,
     })),
-    new winston.transports.File((function(opts) {
-          const newOpts = {};
-          const formatArray = [];
-          const formatOptions = {
-            stringify: () => winston.format((info) => { info.message = JSON.stringify(info.message); })(),
-            formatter: () => winston.format((info) => { info.message = opts.formatter(Object.assign(info, opts)); })(),
-            json: () => winston.format.json(),
-            raw: () => winston.format.json(),
-            label: () => winston.format.label(opts.label),
-            logstash: () => winston.format.logstash(),
-            prettyPrint: () => winston.format.prettyPrint({depth: opts.depth || 2}),
-            colorize: () => winston.format.colorize({level: opts.colorize === true || opts.colorize === 'level', all: opts.colorize === 'all', message: opts.colorize === 'message'}),
-            timestamp: () => winston.format.timestamp(),
-            align: () => winston.format.align(),
-            showLevel: () => winston.format((info) => { info.message = info.level + ': ' + info.message; })()
-          }
-          Object.keys(opts).filter(k => !formatOptions.hasOwnProperty(k)).forEach((k) => { newOpts[k] = opts[k]; });
-          Object.keys(opts).filter(k => formatOptions.hasOwnProperty(k) && formatOptions[k]).forEach(k => formatArray.push(formatOptions[k]()));
-          newOpts.format = winston.format.combine(...formatArray);
-          return newOpts;
-        })({
+    new winston.transports.File(convertOptionsToWinstonV3({
       level: process.env.DBP_FILE_LOG_LEVEL || 'info',
       timestamp: true,
       prettyPrint: true,
@@ -76,3 +36,25 @@ const logger = winston.createLogger({
 module.exports = function getLogger(prefix) {
   return logPrefixer.addPrefixToCommonLogFuncs(logger, prefix);
 };
+
+function convertOptionsToWinstonV3(opts) {
+  const newOpts = {};
+  const formatArray = [];
+  const formatOptions = {
+    stringify: () => winston.format((info) => { info.message = JSON.stringify(info.message); })(),
+    formatter: () => winston.format((info) => { info.message = opts.formatter(Object.assign(info, opts)); })(),
+    json: () => winston.format.json(),
+    raw: () => winston.format.json(),
+    label: () => winston.format.label(opts.label),
+    logstash: () => winston.format.logstash(),
+    prettyPrint: () => winston.format.prettyPrint({depth: opts.depth || 2}),
+    colorize: () => winston.format.colorize({level: opts.colorize === true || opts.colorize === 'level', all: opts.colorize === 'all', message: opts.colorize === 'message'}),
+    timestamp: () => winston.format.timestamp(),
+    align: () => winston.format.align(),
+    showLevel: () => winston.format((info) => { info.message = info.level + ': ' + info.message; })()
+  }
+  Object.keys(opts).filter(k => !formatOptions.hasOwnProperty(k)).forEach((k) => { newOpts[k] = opts[k]; });
+  Object.keys(opts).filter(k => formatOptions.hasOwnProperty(k) && formatOptions[k]).forEach(k => formatArray.push(formatOptions[k]()));
+  newOpts.format = winston.format.combine(...formatArray);
+  return newOpts;
+}
